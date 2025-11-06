b76eb27a8d31647b04e6b3e03b50d5159da77bf0
fmt <6323120@ed.tus.ac.jp>
2025-11-06 06:49:32 +0000

    edit: テストファイルの作成

--- app/backend/api/main.py
+++ app/backend/api/main.py
@@ -16,6 +16,9 @@ def read_items():
 
 # 4. パスパラメータ（/items/5 など）を受け取る
 # {item_id} の部分が動的に変わる
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int):  # 型ヒント(int)で自動的にバリデーション
     return {"item_id": item_id, "description": f"This is item number {item_id}"}
+
